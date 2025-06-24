d8ea4e3d6997885d23ad457933c1fb036bcc7680
baglanadaskhan <baglan.adaskhan@datopian.com>
2025-06-24 16:34:23 +0500

    fix header and footer

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,6 +4,7 @@ import "@/styles/tabs.scss";
 
 import type { AppProps } from "next/app";
 import { DefaultSeo } from "next-seo";
+import { useRouter } from 'next/router';
 
 import SEO from "../next-seo.config";
 
@@ -13,6 +14,17 @@ import ThemeProvider from "../components/theme/theme-provider";
 
 function MyApp({ Component, pageProps }: AppProps) {
   const theme = pageProps.theme || "lighter";
+  const router = useRouter();
+  const isHome = router.pathname === "/";
+  if (isHome) {
+    return (
+      <>
+        <DefaultSeo {...SEO} />
+        <Loader />
+        <Component {...pageProps} />
+      </>
+    );
+  }
   return (
     <ThemeProvider themeName={theme}>
       <DefaultSeo {...SEO} />
